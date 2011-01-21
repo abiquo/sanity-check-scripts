770f847e88a834c05074c400ea17f9a8a1795e7f
Ignasi Barrera <ignasi.barrera@gmail.com>
2011-01-21 12:03:28 +0100

    Added license and documentation

--- abiquoNodeCheckState.py
+++ abiquoNodeCheckState.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
+"""Copyright (c) 2010 Abiquo Holdings
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+__license__ = "MIT http://www.opensource.org/licenses/mit-license.php"
+__author__ = "Ignasi Barrera (ignasi.barrera@abiquo.com)"
+__version__ = "0.1"
+
 import commands
 import ConfigParser
 import os
@@ -8,50 +33,56 @@ from node_check_state.output import *
 # AIM configuration file
 AIM_CONFIG = '/etc/abiquo-aim.ini'
 
-# Configuration parsing functions
+
+# Configuration support functions
 
 def read_config(config_file):
+    """Reads the configuration file"""
     config = ConfigParser.ConfigParser()
     config.readfp(open(config_file))
     config.read(config_file)
     return config
 
+
 # System utility functions
 
 def check_file(file_name):
+    """Checks if the given file exists"""
     label = 'Checking %s ...' % file_name
     test(label, os.path.exists(file_name))
 
 def check_dir(dir_name):
+    """Checks if the given directory exists"""
     label = 'Checking %s ...' % dir_name
     test(label, os.path.isdir(dir_name))
 
 def check_process(name):
+    """Checks if the given process is running"""
     label = 'Checking "%s" process ...' % name
     res = commands.getstatusoutput('ps -e | grep ' + name)
     test(label, not res[0], fail_msg = 'STOPPED')
 
+
 # Check functions
 
 def check_vlan(config):
+    """Checks that all VLAN binaries exist"""
     title('Checking VLAN binaries')
     check_file(config.get('vlan', 'ifconfigCmd'))
     check_file(config.get('vlan', 'vconfigCmd'))
     check_file(config.get('vlan', 'brctlCmd'))
     print
 
-def check_aim(config):
-    title('Checking Abiquo AIM')
+def check_repository(config):
+    """Checks if the Abiquo repository is properly configured"""
     repo = config.get('rimp', 'repository') + '/.abiquo_repository'
     datastore = config.get('rimp', 'datastore')
     check_file(repo)
     check_dir(datastore)
-    check_process('abiquo-aim')
-    print
 
 def check_libvirt(config):
+    """Checks if libvirt is running and can connect to the hypervisor"""
     title('Checking libvirt configuration')
-  
     check_process('libvirtd')
     
     uri = config.get('monitor', 'uri')
@@ -63,6 +94,7 @@ def check_libvirt(config):
     print
 
 def check_hypervisor():
+    """Check if the hypervisor binaries exist"""
     uri = commands.getoutput('virsh uri')
     if uri.startswith('qemu'):
         title('Checking KVM binaries')
@@ -75,6 +107,7 @@ def check_hypervisor():
     print
 
 def check_firewall():
+    """Check if the firewall is active and has filtering rules"""
     title('Checking firewall')
 
     # Check firewall configuration at runlevel
@@ -96,11 +129,24 @@ def check_firewall():
 
     print
 
+def check_aim(config_file):
+    """Checks the configuration values in the system"""
+    title('Checking Abiquo AIM')
+    check_file(config_file)
+
+    if os.path.exists(config_file):
+        config = read_config(config_file)
+        check_repository(config)
+        check_process('abiquo-aim')
+        print
+
+        check_vlan(config)
+        check_libvirt(config)
+    else:
+        print
+       
 if __name__ == '__main__':
-    config = read_config(AIM_CONFIG)
-    check_vlan(config)
-    check_libvirt(config)
-    check_aim(config)
+    check_aim(AIM_CONFIG)
     check_firewall()
     check_hypervisor()
 
--- node_check_state/output.py
+++ node_check_state/output.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python
 
+"""Copyright (c) 2010 Abiquo Holdings
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+__license__ = "MIT http://www.opensource.org/licenses/mit-license.php"
+__author__ = "Ignasi Barrera (ignasi.barrera@abiquo.com)"
+__version__ = "0.1"
+
 
 RESET = "\033[0m"
 GREEN = "\033[1;32m"
 RED = "\033[1;31m"
 YELLOW = "\033[1;33m"
 
+
 def test(label, condition, ok_msg = "OK", fail_msg = "MISSING", warn = False):
+    """Tests the condition and print the result"""
     if condition:
         success(label, ok_msg)
     elif warn:
@@ -15,18 +42,23 @@ def test(label, condition, ok_msg = "OK", fail_msg = "MISSING", warn = False):
         fail(label, fail_msg)
 
 def title(label):
+    """Prints a title"""
     print YELLOW + label + RESET
 
 def success(label, result):
+    """Prints a success flag"""
     print_result(label, GREEN + result + RESET)
 
 def fail(label, result):
+    """Prints a failure flag"""
     print_result(label, RED + result + RESET)
 
 def warning(label, result):
+    """Prints a warning flag"""
     print_result(label, YELLOW + result + RESET)
 
 def print_result(label, result):
+    """Prints the result"""
     print label,
     print "[  %s  ]".rjust(75 - len(label)) % result
 
